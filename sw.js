753595ca1bb1cf0e8f24d1ba44370f3a4435bda0
soyelkhan557-blip <soyelkhan557@gmail.com>
2026-07-22 17:06:13 +0530

    Implement service worker with caching and notifications

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
+const CACHE_NAME = 'cw-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './firebase.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
 
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
+
+self.addEventListener('push', (event) => {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: 'https://via.placeholder.com/192'
+    });
+});

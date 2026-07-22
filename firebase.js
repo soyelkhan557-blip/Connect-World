734c325ce45aabb6ce1a24fd5e0f45beeb3b57fe
soyelkhan557-blip <soyelkhan557@gmail.com>
2026-07-22 10:11:13 +0530

    Initialize Firebase app and export services

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD8qTLYFoy_u9PcVIAmSCunNTXxfp_rEzk",
+  authDomain: "connect-world-8d934.firebaseapp.com",
+  projectId: "connect-world-8d934",
+  storageBucket: "connect-world-8d934.firebasestorage.app",
+  messagingSenderId: "216347498064",
+  appId: "1:216347498064:web:3779448f5faf2512b82355",
+  measurementId: "G-46STVVP6X1"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
